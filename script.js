fc7a546f541bde5ea002750902bad6475a8e3579
Kristen Ruprecht <ksruprec@gmail.com>
2022-02-15 21:15:55 -0500

    html and css created'

---
+++ script.js
@@ -0,0 +1,15 @@
+
+//show "you won!" if:
+//user: rock, computer: scissors
+//user: paper, computer: rock
+//user: scissors: computer: paper
+//otherwise show "you lost!"
+
+//game starts when player chooses a move
+//this triggers countdown (3,2,1) and hands shaking to same cadence
+//after 1, computer's hand changes to random image (r, p, or s)
+//
+const game = () => {
+    
+
+}
